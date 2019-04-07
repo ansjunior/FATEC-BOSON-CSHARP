95ad755e6bbbe8931b66e6eaaf005fc609b5c7d0
Anisio Junior <47663717+ansjunior@users.noreply.github.com>
2019-04-07 05:32:22 -0300

    Add files via upload

---
+++ Variaveis/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 90;
+            bool a = false;
+            int y;
+            y = x;
+
+            Console.WriteLine("O número é " + x.ToString());
+            Console.WriteLine($"E o valor lógico é: {a}");
+            Console.WriteLine($"O número é: {y}");
+        }
+    }
+}
